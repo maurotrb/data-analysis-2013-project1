8534a0fd0e7638c928e6ba3a93ec025922e7a63b
Mauro Taraborelli <MauroTaraborelli@gmail.com>
2013-11-07 21:50:32 +0100

    Munging.

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,7 @@
-# Example preprocessing script.
+# Convert Interest.Rate and Debt.To.Income.Ratio to numeric
+loansData$Interest.Rate <- as.numeric(gsub("%$", "", loansData$Interest.Rate))
+loansData$Debt.To.Income.Ratio <- as.numeric(gsub("%$", "", loansData$Debt.To.Income.Ratio))
+
+# Convert Loan.Length to integer
+loansData$Loan.Length <- as.integer(gsub(" months$", "", loansData$Loan.Length))
+names(loansData)[4] <- "Loan.Length.Months"
