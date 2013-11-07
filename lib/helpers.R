f514b1fa67e8c7e1671e83179227087e7cf96672
Mauro Taraborelli <MauroTaraborelli@gmail.com>
2013-11-08 00:18:32 +0100

    Added helper function capwords.

--- lib/helpers.R
+++ lib/helpers.R
@@ -2,3 +2,12 @@ helper.function <- function()
 {
   return(1)
 }
+
+## "Mixed Case" Capitalizing - toupper( every first letter of a word )
+## from toupper() help
+helper.capwords <- function(s, wsep = " ", strict = FALSE) {
+  cap <- function(s) paste(toupper(substring(s, 1, 1)),
+                           {s <- substring(s, 2); if(strict) tolower(s) else s},
+                           sep = "", collapse = wsep )
+  sapply(strsplit(s, split = wsep), cap, USE.NAMES = !is.null(names(s)))
+}
