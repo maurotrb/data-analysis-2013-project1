6b5244d3b2632bdc4d01a61ca0d2ffc439d2b10e
Mauro Taraborelli <MauroTaraborelli@gmail.com>
2013-11-09 14:58:28 +0100

    Added Rmd to document munging.

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,14 +1,14 @@
-# Convert Interest.Rate and Debt.To.Income.Ratio to numeric
+## @knitr percentage_to_numeric
 loansData$Interest.Rate <- as.numeric(gsub("%$", "", loansData$Interest.Rate))
 loansData$Debt.To.Income.Ratio <- as.numeric(gsub("%$", "", loansData$Debt.To.Income.Ratio))
 
-# Convert Loan.Length to integer
+## @knitr months_to_integer
 loansData$Loan.Length <- as.integer(gsub(" months$", "", loansData$Loan.Length))
 names(loansData)[4] <- "Loan.Length.Months"
 
-# Convert Loan.Purpose values to pascal case with point separator
+## @knitr purpose_to_pc
 loansData$Loan.Purpose <- helper.capwords(loansData$Loan.Purpose, "_")
 loansData$Loan.Purpose <- gsub("_", ".", loansData$Loan.Purpose)
 
-# Convert Home.Ownership values to pascal case
+## @knitr ownership_to_pc
 loansData$Home.Ownership <- helper.capwords(loansData$Home.Ownership, strict=TRUE)
